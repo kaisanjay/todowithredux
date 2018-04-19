c5d7e378eb3c6f03080c0cc7919c4d8d325d4793
kaisanjay <sanjaykai.1994@gmail.com>
2018-04-20 03:10:36 +0530

    set up reducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-	<App /> , document.getElementById("root"))
+	<Provider store={store}>
+	  <App />
+	</Provider> , document.getElementById("root"))
