f45e8ebf02b7f9a59214b7b137ff83be5fbf6f01
kaisanjay <sanjaykai.1994@gmail.com>
2018-04-21 13:07:38 +0530

    Adding task completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import '../index.css'
 import { addReminder } from '../actions';
 
 class App extends React.Component {
@@ -17,8 +18,27 @@ class App extends React.Component {
 		this.props.addReminder(this.state.text);
 	}
 
+	renderReminders() {
+		const { reminders } = this.props;
+		return (
+			<ul className="list-group col-sm-4">
+			  {
+			  	reminders.map(reminder => {
+			  		return (
+			  			<li key={reminder.id} className="list-group-item">
+			  			  <div>
+			  			    {reminder.text}
+			  			  </div>
+			  			</li>
+			  			)
+			  	})
+			  }
+			</ul>
+			)
+		
+	}
+
 	render(){
-		console.log('this.props', this.props);
 		return (
 			<div className="App">
 				<div className="title">
@@ -26,25 +46,26 @@ class App extends React.Component {
 
 				</div>
 				<div className="form-inline">
-				 <div className="form-group">
-				  <input 
-				     className="form-control"
-				     placeholder=" I have to..."
-				     onChange={this.handleChange} 
-				   />
-				 </div>
-				 <div>
-				 <button
-				    type="button"
-				    className="btn btn-success"
-				    onClick={this.addReminder}
-				    >
-				    Add Reminder
-				 </button>
-				 </div>
+					 <div className="form-group">
+					  <input 
+					     className="form-control"
+					     placeholder=" I have to..."
+					     onChange={this.handleChange} 
+					   />
+					 </div>
+					 <div>
+					 <button
+					    type="button"
+					    className="btn btn-success"
+					    onClick={this.addReminder}
+					    >
+					    Add Reminder
+					 </button>
+					 </div>
 
 
 				</div>
+				  {this.renderReminders()}
 			</div>
 
 			)
