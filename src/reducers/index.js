fa22d129f728b9cc0dddd1b45f6be2ebd72b3df0
kaisanjay <sanjaykai.1994@gmail.com>
2018-04-23 01:37:21 +0530

    3. reducer CLEAR_REMINDERS

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDERS } from '../constants';
 import { bake_cookie, read_cookie } from 'sfcookies';
 const reminder = (action) => {
 	let { text, dueDate } = action;
@@ -28,6 +28,10 @@ const reminders = (state = [], action) => {
 		   reminders = removeById(state, action.id);
 		   bake_cookie('reminders', reminders);
 		   return reminders;
+		case CLEAR_REMINDERS:
+		    reminders = [];
+		    bake_cookie('reminders', reminders);
+		    return reminders;
 
 		default:
 		  return state;
